a608655436be92ff4fb63098ee2879ebbcbbda31
tobbbii <tobiidowu715@gmail.com>
2023-09-14 12:56:23 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints numbers excluding 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != 2 && i != 4)
+			putchar(i = '0');
+	}
+	putchar('\n');
+}
