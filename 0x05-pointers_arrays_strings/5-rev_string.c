f8c1ae7eb10ebde6684157430f0bd920236afa37
tobbbii <tobiidowu715@gmail.com>
2023-09-18 10:41:50 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ void rev_string(char *s)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for ((i = 0 ; i < 0 ; i < count / 2); i++)
+	for (i = 0 ; i < count / 2 ; i++)
 	{
 		j = s[i];
 		s[i] = s[count - 1 - i];
