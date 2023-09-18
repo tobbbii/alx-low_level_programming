1078dc527072cef7d2b4b35d900c3b60e3857ff7
tobbbii <tobiidowu715@gmail.com>
2023-09-18 12:00:43 -0700

    task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@
 
 void puts_half(char *str)
 {
-	int i, n;
+	int i;
 	int count = 0;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
@@ -16,9 +16,25 @@ void puts_half(char *str)
 		count++;
 
 	}
+	if (count % 2 == 0)
+	{
+	int n;
+
 	for (n = count / 2 ; n <= count ; n++)
 	{
 		_putchar(str[n]);
+	}
+	}
+	else
+	{
+		{
+	int n;
+
+	for (n = (count - 1) / 2 ; n <= count ; n++)
+	{
+		_putchar(str[n]);
+	}
+		}
 	}
 	_putchar('\n');
 }
