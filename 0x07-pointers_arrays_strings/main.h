0aaccc0f1c68d69d0f3b326811dc70b0c0632d3d
tobbbii <tobiidowu715@gmail.com>
2023-09-25 13:28:11 -0700

    task8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -10,5 +11,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
