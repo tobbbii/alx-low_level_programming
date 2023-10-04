e24247a3f61cb536e1450b853ac6fb1624169877
tobbbii <tobiidowu715@gmail.com>
2023-10-04 08:04:14 -0700

    task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,12 +30,15 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0 ; i < ac ; i++)
 	{
-		str[r] = av[i][n];
-		r++;
-	}
+		for (n = 0 ; av[i][n] ; n++)
+		{
+			str[r] = av[i][n];
+			r++;
+		}
 	if (str[r] == '\0')
 	{
 		str[r++] = '\n';
+	}
 	}
 	return (str);
 }
