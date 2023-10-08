a40a4b252b093e6eba1fcd7823d68b8ef22f0f9e
tobbbii <tobiidowu715@gmail.com>
2023-10-08 05:28:32 -0700

    correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	l = max - min + 1;
-	a = malloc(sizeof(int) * 1);
+	a = malloc(sizeof(int) * l);
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < l; i++)
