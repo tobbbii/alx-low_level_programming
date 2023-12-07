e7d0c2435a670eb01d28f1c6acd063a0e46e88b5
tobbbii <tobiidowu715@gmail.com>
2023-12-07 14:09:28 -0800

    task 14

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,12 +4,14 @@
  * @head: Pointer to the head of the list
  */
 void free_dlistint(dlistint_t *head)
+
 {
-    dlistint_t *temp;
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+	temp = head;
+	head = head->next;
+	free(temp);
+	}
 }
