d744ab43768d00855542996cec1ab58cb7d02183
tobbbii <tobiidowu715@gmail.com>
2023-12-07 15:17:42 -0800

    task20

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,38 +8,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current, *temp = *head;
-    unsigned int i = 0;
+	dlistint_t *current, *temp = *head;
+	unsigned int i = 0;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+	{	return (-1);
+	}
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(temp);
-        return (1);
-    }
+	if (index == 0)
+	{
+	*head = (*head)->next;
+	if (*head != NULL)
+	(*head)->prev = NULL;
+	free(temp);
+	{	return (1);
+	}
+	}
 
-    while (i < index - 1)
-    {
-        if (temp == NULL)
-            return (-1);
-        temp = temp->next;
-        i++;
-    }
+	while (i < index - 1)
+	{
+	if (temp == NULL)
+	{	return (-1);
+	}
+	temp = temp->next;
+	i++;
+	}
 
-    if (temp == NULL || temp->next == NULL)
-        return (-1);
+	if (temp == NULL || temp->next == NULL)
 
-    current = temp->next->next;
-    free(temp->next);
-    temp->next = current;
+	{	return (-1);
+	}
 
-    if (current != NULL)
-        current->prev = temp;
+	current = temp->next->next;
+	free(temp->next);
+	temp->next = current;
 
-    return (1);
+	if (current != NULL)
+	current->prev = temp;
+
+	{	return (1);
+	}
 }
